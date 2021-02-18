a5881796ec90cfb3fe9b66939dfed9f96af47876
Gabo-utn <64754786+Gabo-utn@users.noreply.github.com>
2021-02-18 13:33:27 -0300

    Add files via upload
    18

--- public/index.php
+++ public/index.php
@@ -42,6 +42,10 @@ include_once "globales.php";
 /************* API *************/
 
 include_once 'controllers/grupo.php';
+include_once 'controllers/tarea.php';
+include_once 'controllers/servicio.php';
+include_once 'controllers/servicio_tarea.php';
+include_once 'controllers/grupo_servicio.php';
 
 
 $app->get('/', function (Request $request, Response $response, $args) {
